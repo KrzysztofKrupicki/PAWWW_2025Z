7fb1688c366f0e961946b084b5c2e9fdd419741f
KrzysztofKrupicki <147048678+KrzysztofKrupicki@users.noreply.github.com>
2025-10-27 16:00:29 +0100

    Lab 4

---
+++ lab4/pages/test_include.php
@@ -0,0 +1,7 @@
+<?php
+$test_include_var = 452;
+function test_include() {
+    $test_include_var = 420;
+    echo "<p>Test metody include $test_include_var</p>";
+}
+?>
---
+++ lab4/pages/test_require_once.php
@@ -0,0 +1,7 @@
+<?php
+$test_require_once_var = 254;
+function test_require_once() {
+    $test_require_once_var = 24;
+    echo "<p>Test metody require_once $test_require_once_var</p>";
+}
+?>
